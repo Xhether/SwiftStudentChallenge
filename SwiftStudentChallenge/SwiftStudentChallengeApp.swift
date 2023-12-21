5da9f42fdfea564c8121c237ceca154f8a105355
Charles <60122616+Xhether@users.noreply.github.com>
2023-12-20 21:11:04 -0800

    Initial Commit

---
+++ SwiftStudentChallenge/SwiftStudentChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStudentChallengeApp.swift
+//  SwiftStudentChallenge
+//
+//  Created by Charles Liggins on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftStudentChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
